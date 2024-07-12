7eb7be1118874367575c0395df50d8a3f42d419e
akopyan-arsen <149099397+akopyan-arsen@users.noreply.github.com>
2024-07-12 14:23:52 +0300

    error fix

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,7 +1,8 @@
 import css from "./ErrorMessage.module.css";
+import { ErrorMessageProps } from "./ErrorMessage.type";
 
-const ErrorMessage: React.FC<{ error: Error }> = ({ error }) => {
-  return <div className={css.error}>{error.message}</div>;
+const ErrorMessage: React.FC<ErrorMessageProps> = ({ error }) => {
+  return <div className={css.error}>{error}</div>;
 };
 
 export default ErrorMessage;

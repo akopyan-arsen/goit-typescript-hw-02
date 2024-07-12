001964cc3af61effd404df09d5d29543be3bb91b
akopyan-arsen <149099397+akopyan-arsen@users.noreply.github.com>
2024-07-12 14:15:30 +0300

    Update ErrorMessage.tsx

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,6 +1,6 @@
 import css from "./ErrorMessage.module.css";
 
-const ErrorMessage = ({ error }) => {
+const ErrorMessage: React.FC<{ error: Error }> = ({ error }) => {
   return <div className={css.error}>{error.message}</div>;
 };
 
